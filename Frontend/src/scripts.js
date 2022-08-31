dbb59bc496e121188f82eb0dad98986d0f3a5d78
Thais Esteves <thaisdossantosesteves@gmail.com>
2022-08-31 14:49:42 -0300

    update funções javascript

--- Frontend/src/scripts.js
+++ Frontend/src/scripts.js
@@ -1,3 +1,6 @@
+
+// CADASTRO
+
 const formCadastro = document.getElementById('cadastro-form');
 
 // FUNÇÃO QUE CADASTRA O CLIENTE EM UMA LISTA
@@ -9,7 +12,17 @@ const formCadastro = document.getElementById('cadastro-form');
 
 });*/
 
-
+ /*let data = {
+    "document": ,
+    "name": ,
+    "birthDate": ,
+    "email": 
+        "numero": inputs["numeroCartao"].value,
+        "vencimento": inputs["vencimento"].value,  // alterar para botar as infos de mudança
+        "cvv": inputs["cvv"].value
+    }
+    
+}*/
 
 // VALIDAÇÕES
 
@@ -76,7 +89,10 @@ function validarCpf(cpf){
     } else if (cpf.value.length < 11) {
         errorValidation(cpf, 'O CPF deve ter 11 caracteres')
 
-    } else {
+    } else if (cpf.value.length > 11) {
+        errorValidation(cpf, 'O CPF deve ter 11 caracteres')
+
+    }else {
         successValidation(cpf)
     }
 }
@@ -168,3 +184,180 @@ function successValidation(input){
     formControl.className = 'form-control success'
 }
 
+
+
+// ALTERAR
+
+
+function checkId(input){
+    if(input.value === ''){
+        errorValidation(input, 'Campo Obrigatório')
+
+    } else if (!onlyNumbers.test(input.value.trim())){
+        errorValidation(input, 'Este campo não aceita letras nem caracteres especiais')
+
+    } else if (input.value.length < 11) {
+        errorValidation(input, 'O CPF deve ter 11 caracteres')
+
+    } else if (input.value.length > 11) {
+        errorValidation(input, 'O CPF deve ter 11 caracteres')
+
+    }else {
+        successValidation(input)
+        return true
+    }
+    return false
+}
+
+const formAlterarCliente= document.getElementById("formAlterar")
+const idCliente = document.getElementById("idCliente")
+const btnPesquisar = document.getElementById("btn-pesquisar")
+
+formAlterarCliente.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkId(idCliente)
+});
+                                                              // MUDAR 
+
+
+btnPesquisar.addEventListener("click", function(){
+    if (idCliente.value.length > 0){
+         getClient(idCliente.value) //botar onde pesquisar
+        idCliente.value = "";
+    } else{
+        formAlterarCliente.classList.toggle("actived")
+    }
+})
+
+
+
+// REMOVER
+
+const formRemoverCliente= document.getElementById("formRemover")
+const idClienteRemove = document.getElementById("idClienteRemove")
+const btnRemover = document.getElementById("btn-remover")
+
+formRemoverCliente.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkId(idClienteRemove)
+});
+
+
+
+// MOSTRAR CLIENTE
+const clientList = document.getElementById('clienteInfo');
+
+async function getClient (id) {
+    
+    let payload = {}
+    let response = await fetch("http://127.0.0.1:8000/client/" + id, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((r) => r.json())
+    .then((data) => payload = data)
+    console.log("payload", payload)
+    console.log("response", response)
+
+    let documentCliente = payload["document"]
+    let dataNascimento = payload["birthDate"]
+    let nome = payload["name"]
+    let email  = payload["email"]
+    let creditCardCVV = payload["creditCard"]["cvv"]
+    let creditCardExpiration = payload["creditCard"]["expiration"]
+    let creditCardNumber = payload["creditCard"]["number"]
+
+    const container = document.createElement('ul');
+	const novaInfo = document.createElement('li');
+	const infoCpf = document.createTextNode('CPF: ' + documentCliente);
+    const infoDataDeNascimento  = document.createTextNode('Data de Nascimento: ' + dataNascimento);
+	const infoNome = document.createTextNode('Nome: ' + nome);
+	const infoEmail = document.createTextNode('Email: ' + email);
+	const infoCreditCardCvv = document.createTextNode('CVV: ' + creditCardCVV);
+	const infoCreditCardNumber = document.createTextNode('Vencimento: ' + creditCardExpiration);
+	const infoCreditCardExpiration = document.createTextNode('Número: ' + creditCardNumber);
+
+    
+	novaInfo.setAttribute('for', nome);
+    novaInfo.appendChild(infoNome);
+    
+	novaInfo.setAttribute('for', documentCliente);
+	novaInfo.appendChild(infoCpf);
+
+    novaInfo.setAttribute('for', dataNascimento);
+	novaInfo.appendChild(infoDataDeNascimento);
+
+    novaInfo.setAttribute('for', email);
+	novaInfo.appendChild(infoEmail);
+
+    novaInfo.setAttribute('for', creditCardNumber);
+	novaInfo.appendChild(infoCreditCardNumber);
+
+    novaInfo.setAttribute('for', creditCardCVV);
+	novaInfo.appendChild(infoCreditCardCvv);
+
+    novaInfo.setAttribute('for', creditCardExpiration);
+	novaInfo.appendChild(infoCreditCardExpiration);
+
+
+	container.classList.add('info');
+	container.appendChild(novaInfo);
+
+
+	clientList.appendChild(container);
+    
+
+}
+
+
+const formMostrarCliente= document.getElementById("formCliente")
+const idClienteInfo = document.getElementById("idClienteInfo")
+const btnMostrarCliente = document.getElementById("btn-mostrarCliente")
+
+formMostrarCliente.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let sucesso = checkId(idClienteInfo)
+    if (sucesso){
+        getClient(idClienteInfo.value) 
+    }
+});
+
+/*btnMostrarCliente.addEventListener("click", function(){
+    if (idClienteInfo.value.length > 0){
+         getClient(idClienteInfo.value) 
+        idClienteInfo.value = "";
+    } else{
+        formMostrarCliente.classList.toggle("actived")
+    }
+})*/
+
+
+
+
+
+
+// MOSTRAR TODOS OS CLIENTES
+
+const btnMostrarTodosClientes = document.getElementById("btn-mostrarTodosClientes")
+
+async function getAllClients () {
+  
+    const response = await fetch("http://127.0.0.1:8000/clients/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+    console.log("Request successful!")
+    console.log(response.json())
+  }
+
+  btnMostrarTodosClientes.addEventListener("click", function(){
+    getAllClients() 
+})
